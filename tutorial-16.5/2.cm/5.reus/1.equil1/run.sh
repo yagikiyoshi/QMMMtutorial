19f4a76d350bb3a6caa26fe640f7a4ea053df0ac
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2022-03-25 00:19:53 +0900

    REUS for cm

---
+++ tutorial-16.5/2.cm/5.reus/1.equil1/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+export LD_LIBRARY_PATH=/path/to/qsimulate/lib:$LD_LIBRARY_PATH
+export PATH=$PATH:/path/to/genesis/bin
+
+export OMP_NUM_THREADS=4
+export BAGEL_NUM_THREADS=${OMP_NUM_THREADS}
+export MKL_NUM_THREADS=${OMP_NUM_THREADS}
+export I_MPI_PERHOST=4
+export I_MPI_DEBUG=5
+
+./geninp.sh
+
+for i in `seq 1 21`; do
+  mpiexec.hydra -n 2 atdyn equil1_${i}.inp >& equil1_${i}.out
+done
+
+exit 0
+

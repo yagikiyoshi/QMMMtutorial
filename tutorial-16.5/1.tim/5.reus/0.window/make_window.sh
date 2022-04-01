d3a0f5489907cad80ece6c0859a840e6100689bb
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2022-04-02 02:55:08 +0900

    minor fix

--- tutorial-16.5/1.tim/5.reus/0.window/make_window.sh
+++ tutorial-16.5/1.tim/5.reus/0.window/make_window.sh
@@ -3,5 +3,4 @@
 gfortran make_window.f90  -o make_window
 
 # change rpath_xx.dat to your final path
-#./make_window -dat ../../4.mep/2.analysis/rpath_xx.dat -ds 0.1 -ndim 5 -idx 1,2,3,4,6 > win_rr.log
-./make_window -dat ../../4.mep/2.analysis/rpath_92.dat -ds 0.1 -ndim 5 -idx 1,2,3,4,6 > win_rr.log
+./make_window -dat ../../4.mep/2.analysis/rpath_xx.dat -ds 0.1 -ndim 5 -idx 1,2,3,4,6 > win_rr.log

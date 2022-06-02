1526d70d8a8f5149e2cbf55a3f0365e208854828
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2022-06-02 22:41:25 +0900

    minor

--- tutorial-16.4/3_harmonic/run.sh
+++ tutorial-16.4/3_harmonic/run.sh
@@ -6,6 +6,6 @@ export OMP_NUM_THREADS=8
 export QM_NUM_THREADS=8
 mpirun -np 2 atdyn qmmm_harm.inp  > qmmm_harm.log  2>&1
 
-. ../sindo/sindo-4.0_220319/sindovars.sh
+. ../sindo/sindo-4.0/sindovars.sh
 java HarmSpectrum qmmm_harm.minfo 5 300 1800 1 > harmonic.spectrum
 gnuplot plotIR.gpi

f5d9cfc6bb1d80cb5af2d6d9b2c25f97c625ecf0
Kiyoshi Yagi <kiyoshi.yagi@riken.jp>
2022-06-07 09:28:01 +0900

    added OpenMPI and IntelMPI

--- tutorial-16.4/3_harmonic/run.sh
+++ tutorial-16.4/3_harmonic/run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-export PATH=$PATH:../bin
+export PATH=/path/to/genesis/bin:$PATH
 
 export OMP_NUM_THREADS=8
 export QM_NUM_THREADS=8
-mpirun -np 2 atdyn qmmm_harm.inp  > qmmm_harm.log  2>&1
+
+# 1) Open MPI
+#
+mpirun -np 2 --map-by node:pe=${QM_NUM_THREADS} atdyn qmmm_min.inp > qmmm_min.out 2>&1
+
+# 2) Intel MPI
+#
+#mpirun -np 2 -ppn 2 atdyn qmmm_harm.inp  > qmmm_harm.log  2>&1
 
 . ../sindo/sindo-4.0/sindovars.sh
 java HarmSpectrum qmmm_harm.minfo 5 300 1800 1 > harmonic.spectrum
